51f05610bd9e0d71cdc75dc5fbe74307826a3ab0
Mayank Goswami <91790864+hacker-4-good@users.noreply.github.com>
2022-05-27 22:37:45 +0530

    Add files via upload

--- code.py
+++ code.py
@@ -1,57 +1,29 @@
-import pygame
-import random
+import cv2 
+import pickle
+width,height  = 107,46
+posList = []
 
-#intialise the pygame
-pygame.init()
+try:
+    with open("carParkPos","rb") as f:
+        posList = pickle.load(f)
+except:
+    posList = []
 
-#create the screen 
-screen = pygame.display.set_mode((800,600))
+def mouseClick(events,x,y,flags,params):
+    if events == cv2.EVENT_LBUTTONDOWN:
+        posList.append((x,y))
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i,pos in enumerate(posList):
+            x1,y1 = pos
+            if x1<x<x1+width and y1<y<y1+height:
+                posList.pop(i)
 
-#title and icons
-pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load("ufo.png")
-pygame.display.set_icon(icon)
-
-#player
-playerImg = pygame.image.load("player.png")
-playerX = 370
-playerY = 480
-playerX_change = 0
-
-#enemy
-enemyImg = pygame.image.load("enemy.png")
-enemyX = random.randint(0,800)
-enemyY = random.randint(50,150)
-enemyX_change = 0
-
-def player(x,y):
-    screen.blit(playerImg, (x,y))
-
-def enemy(x,y):
-    screen.blit(enemyImg, (x,y))
-
-#game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        #if keystroke is pressed check whether it is right or left
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playerX_change = -0.2
-            if event.key == pygame.K_RIGHT:
-                playerX_change = 0.2
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                playerX_change = 0
-    # RGB = red, blue, green
-    playerX += playerX_change
-    if playerX<=0:
-        playerX = 0
-    elif playerX>=736:
-        playerX = 736
-    screen.fill((0,0,0))
-    player(playerX,playerY)
-    enemy(enemyX,enemyY)
-    pygame.display.update()
+    with open("carParkPos","wb") as f:
+        pickle.dump(posList,f)
+while True:
+    img = cv2.imread("carParkImg.png")
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0]+width, pos[1]+height), (255,0,255), 2)
+    cv2.imshow("Image",img)
+    cv2.setMouseCallback("Image",mouseClick)
+    cv2.waitKey(1)
